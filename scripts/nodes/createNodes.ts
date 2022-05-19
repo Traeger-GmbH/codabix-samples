3a931d13a0cee024117ffa3694cea1b3d68dd683
d.scharnagl <d.scharnagl@traeger.de>
2022-05-19 13:39:23 +0200

    Fixed typos

--- scripts/nodes/createNodes.ts
+++ scripts/nodes/createNodes.ts
@@ -1,13 +1,13 @@
 // Copyright (c) Traeger Industry Components GmbH. All Rights Reserved.
 // 
-// The following snippet demonstrates how create nodes in a script.
+// The following snippet demonstrates how to create nodes in a script.
 // 
 runtime.handleAsync(async function () {
     let parentNode = codabix.findNode("/Nodes/MyParent", true);
     let nodeNames = [ "first", "second" ];
 
     for (let i = 0; i < nodeNames.length; i++) {
-        let inputArgumentNode = codabix.createNode({
+        let newNode = codabix.createNode({
                 name: nodeNames[i],
                 parentIdentifier: parentNode.identifier,
                 type: codabix.NodeTypeEnum.Value,
